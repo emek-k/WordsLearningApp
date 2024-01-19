f0130d03c132105a9a22884f2cb8d15660345444
kacperx0m <125907725+kacperx0m@users.noreply.github.com>
2024-01-19 20:51:46 +0100

    Create MainTestModelu.java

---
+++ src/main/java/edu/pb/MainTestModelu.java
@@ -0,0 +1,46 @@
+package edu.pb;
+
+import edu.pb.model.Dictionary;
+import edu.pb.model.factories.EnglishWordFactory;
+import edu.pb.model.factories.IWordFactory;
+import edu.pb.model.factories.PolishWordFactory;
+import edu.pb.model.words.Word;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Set<String> set = new HashSet<>(Arrays.asList("pociag", "zug", "costam"));
+//        IWordFactory factory = new EnglishWordFactory();
+//        Word word = factory.createWord("train", "srodek lokomocji", "pociag");
+//
+//        System.out.println(word.name);
+
+        Set<String> supportedLanguages = new HashSet<>(Arrays.asList("English", "Polish", "German"));
+
+        Dictionary dictionary = new Dictionary(supportedLanguages);
+        IWordFactory englishFactory = new EnglishWordFactory();
+        IWordFactory polishFactory = new PolishWordFactory(); // Zakładając, że istnieje taka klasa
+
+        dictionary.addWord("apple", englishFactory.createWord("apple", "A fruit", "jablko"));
+        dictionary.addWord("jabłko", polishFactory.createWord("jabłko", "Owoc", "apple"));
+        
+        Word word = dictionary.findWord("apple");
+        if (word != null) {
+            System.out.println("Found word: " + word.name + " meaning " + word.translation);
+        }
+    }
+
+    //if language == english:
+    //WordsFactory factory= new EnglishWordFactory
+
+    //else if lang==german:
+    //factory = new german...
+
+    //factory.createWord()
+
+    //jak chcemy dodac nowa klase to latwo
+}
