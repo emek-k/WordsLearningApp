d50aa82440380361695a40dbd6445926aa63f5f4
Dawid <daw1d99@o2.pl>
2024-01-21 00:37:26 +0100

    Dodanie endpointow dla slownikow

--- src/main/java/edu/pb/Main.java
+++ src/main/java/edu/pb/Main.java
@@ -17,6 +17,20 @@ public class Main {
         // Dodaj obsługę ścieżki "/api/words"
         server.createContext("/api/words", new MyHandler());
 
+
+        server.createContext("/api/dictionary/pol-eng", new DictionaryHandler("pol-eng"));
+
+
+        server.createContext("/api/dictionary/pol-fra", new DictionaryHandler("pol-fra"));
+
+        server.createContext("/api/dictionary/pol-ger", new DictionaryHandler("pol-ger"));
+
+        server.createContext("/api/dictionary/eng-pol", new DictionaryHandler("eng-pol"));
+
+        server.createContext("/api/dictionary/fra-pol", new DictionaryHandler("fra-pol"));
+
+        server.createContext("/api/dictionary/ger-pol", new DictionaryHandler("ger-pol"));
+
         // Dodaj obsługę ścieżki "/"
         server.createContext("/", new RootHandler());
 
@@ -31,6 +45,7 @@ public class Main {
     }
 
     static class MyHandler implements HttpHandler {
+
         @Override
         public void handle(HttpExchange t) throws IOException {
             // Tutaj możesz obsłużyć żądanie HTTP i zwrócić odpowiedź
@@ -41,7 +56,23 @@ public class Main {
             os.close();
         }
     }
+    static class DictionaryHandler implements HttpHandler {
+        private final String dictionaryName;
 
+        public DictionaryHandler(String dictionaryName) {
+            this.dictionaryName = dictionaryName;
+        }
+
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            // Obsługa zapytania dla danego słownika
+            String response = "Hello, this is your response for the " + dictionaryName + " dictionary!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
     static class RootHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
